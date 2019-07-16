63324bfde52a9ff383c002a5a715cdf03ff11b21
Shuo Han <shan50@jhu.edu>
2019-07-16 13:46:03 -0400

    Debug norm
    Use view and permutation in BatchNorm. But since during the training,
    BatchNorm uses the sample mean and variance so view and permuation are
    not required.

--- pytorch_reflection/norm.py
+++ pytorch_reflection/norm.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import torch
 from torch.nn.modules.batchnorm import _BatchNorm
 from torch.nn.modules.instancenorm import _InstanceNorm
 
@@ -14,9 +15,11 @@ class LRBatchNorm2d(_BatchNorm):
                              .format(input.dim()))
 
     def forward(self, input):
-        num_samples, num_channels, x, y = input.shape
-        input = input.view(num_samples, 2, -1, x, y).permute(0, 2, 1, 3, 4)
-        return super().forward(input)
+        n, _, x, y = input.shape
+        input = input.view(n, 2, -1, x, y).permute(0, 2, 1, 3, 4)
+        output = super().forward(input)
+        output = output.permute(0, 2, 1, 3, 4).contiguous().view(n, -1, x, y)
+        return output
 
 
 class LRBatchNorm3d(_BatchNorm):
@@ -29,38 +32,51 @@ class LRBatchNorm3d(_BatchNorm):
                              .format(input.dim()))
 
     def forward(self, input):
-        num_samples, num_channels, x, y, z = input.shape
-        input = input.view(num_samples, 2, -1, x, y, z)
-        input = input.permute(0, 2, 1, 3, 4, 5)
-        return super().forward(input)
+        n, _, x, y, z = input.shape
+        input = input.view(n, 2, -1, x, y, z).permute(0, 2, 1, 3, 4, 5)
+        output = super().forward(input)
+        output = output.permute(0, 2, 1, 3, 4, 5).contiguous()
+        output = output.view(n, -1, x, y, z)
+        return output
 
 
-class LRInstanceNorm2d(_InstanceNorm):
+class _LRInstanceNorm(_InstanceNorm):
+    """Left-right reflection equivariant instance normalization
+
+    """
+    def _check_input_dim(self, input):
+        if input.dim() != 4:
+            raise ValueError('expected 4D input (got {}D input)'
+                             .format(input.dim()))
+
+    def forward(self, input):
+        orig_weight = self.weight
+        orig_bias = self.bias
+        weight = torch.cat((self.weight, self.weight), 0)
+        self.weight = torch.nn.Parameter(weight)
+        bias = torch.cat((self.bias, self.bias), 0)
+        self.bias = torch.nn.Parameter(bias)
+        output = super().forward(input)
+        self.weight = orig_weight
+        self.bias = orig_bias
+        return output
+
+
+class LRInstanceNorm2d(_LRInstanceNorm):
     """Left-right reflection equivariant instance normalization 2D
+
+    """
+    def _check_input_dim(self, input):
+        if input.dim() != 4:
+            raise ValueError('expected 4D input (got {}D input)'
+                             .format(input.dim()))
+
+
+class LRInstanceNorm3d(_LRInstanceNorm):
+    """Left-right reflection equivariant instance normalization 3D
 
     """
     def _check_input_dim(self, input):
         if input.dim() != 5:
             raise ValueError('expected 5D input (got {}D input)'
                              .format(input.dim()))
-
-    def forward(self, input):
-        num_samples, num_channels, x, y = input.shape
-        input = input.view(num_samples, 2, -1, x, y).permute(0, 2, 1, 3, 4)
-        return super().forward(input)
-
-
-class LRInstanceNorm3d(_InstanceNorm):
-    """Left-right reflection equivariant instance normalization 3D
-
-    """
-    def _check_input_dim(self, input):
-        if input.dim() != 6:
-            raise ValueError('expected 6D input (got {}D input)'
-                             .format(input.dim()))
-
-    def forward(self, input):
-        num_samples, num_channels, x, y, z = input.shape
-        input = input.view(num_samples, 2, -1, x, y, z)
-        input = input.permute(0, 2, 1, 3, 4, 5)
-        return super().forward(input)
--- tests/conv2.py
+++ tests/conv2.py
@@ -8,41 +8,58 @@ import matplotlib.pyplot as plt
 from torch.nn.functional import relu
 
 from pytorch_reflection.conv import LRConvI2F2d, LRConvF2F2d, LRConvF2I2d
+from pytorch_reflection.norm import LRInstanceNorm2d, LRBatchNorm2d
 
 
 class Net1(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.conv1 = LRConvI2F2d(in_channels, 2, 3, padding=1)
-        self.conv2 = LRConvF2F2d(2, 4, 3, padding=1)
-        self.conv3 = LRConvF2F2d(4, 8, 3, padding=1)
+        self.conv1 = LRConvI2F2d(in_channels, 2, 3, padding=1, use_bias=False)
+        # self.norm1 = LRInstanceNorm2d(2, affine=True)
+        self.norm1 = LRBatchNorm2d(2, affine=True)
+        self.conv2 = LRConvF2F2d(2, 4, 3, padding=1, use_bias=False)
+        # self.norm2 = LRInstanceNorm2d(4, affine=True)
+        self.norm2 = LRBatchNorm2d(4, affine=True)
+        self.conv3 = LRConvF2F2d(4, 8, 3, padding=1, use_bias=False)
+        # self.norm3 = LRInstanceNorm2d(8, affine=True)
+        self.norm3 = LRBatchNorm2d(8, affine=True)
         self.conv4 = LRConvF2I2d(8, out_channels, 3, padding=1)
 
     def forward(self, input):
         output = self.conv1(input)
+        output = self.norm1(output)
         output = relu(output)
         output = self.conv2(output)
+        output = self.norm2(output)
         output = relu(output)
         output = self.conv3(output)
+        output = self.norm3(output)
         output = relu(output)
         output = self.conv4(output)
+        print(self.norm3.running_mean)
         return output
 
 
 class Net2(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.conv1 = torch.nn.Conv2d(in_channels, 4, 3, padding=1)
-        self.conv2 = torch.nn.Conv2d(4, 8, 3, padding=1)
-        self.conv3 = torch.nn.Conv2d(8, 16, 3, padding=1)
+        self.conv1 = torch.nn.Conv2d(in_channels, 4, 3, padding=1, bias=False)
+        self.norm1 = torch.nn.BatchNorm2d(4)
+        self.conv2 = torch.nn.Conv2d(4, 8, 3, padding=1, bias=False)
+        self.norm2 = torch.nn.BatchNorm2d(8)
+        self.conv3 = torch.nn.Conv2d(8, 16, 3, padding=1, stride=2, bias=False)
+        self.norm3 = torch.nn.BatchNorm2d(16)
         self.conv4 = torch.nn.Conv2d(16, out_channels, 3, padding=1)
 
     def forward(self, input):
         output = self.conv1(input)
+        output = self.norm1(output)
         output = relu(output)
         output = self.conv2(output)
+        output = self.norm2(output)
         output = relu(output)
         output = self.conv3(output)
+        output = self.norm3(output)
         output = relu(output)
         output = self.conv4(output)
         return output
@@ -60,6 +77,11 @@ flipped_data = data.flip(2)
 net1 = Net1(1, 1)
 output1 = net1(data).detach().numpy()
 output1_r = net1(flipped_data).detach().numpy()
+
+net1 = net1.eval()
+output1 = net1(data).detach().numpy()
+output1_r = net1(flipped_data).detach().numpy()
+
 diff1 = np.abs(output1[0, 0, ::-1, ...] - output1_r[0, 0, ...])
 
 plt.figure()
