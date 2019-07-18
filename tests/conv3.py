f5b2acb820dd07ea2e6142111446f7747dc7f492
Shuo Han <shan50@jhu.edu>
2019-07-17 21:40:41 -0400

    Upgrade norm
    Pytorch norm does not support 6D tensor. Reshape the group into the
    first spatial dim.

--- tests/conv3.py
+++ tests/conv3.py
@@ -8,22 +8,35 @@ import matplotlib.pyplot as plt
 from torch.nn.functional import relu
 
 from pytorch_reflection.conv import LRConvI2F3d, LRConvF2F3d, LRConvF2I3d
+from pytorch_reflection.norm import LRInstanceNorm3d, LRBatchNorm3d
 
+
+Norm = LRInstanceNorm3d
+affine = True 
 
 class Net1(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.conv1 = LRConvI2F3d(in_channels, 2, 3, padding=1)
+        self.norm1 = Norm(2, affine=affine)
+        # self.norm1 = torch.nn.GroupNorm(2, 2 * 2, affine=True)
         self.conv2 = LRConvF2F3d(2, 4, 3, padding=1)
+        self.norm2 = Norm(4, affine=affine)
+        # self.norm2 = torch.nn.GroupNorm(4, 2 * 4, affine=True)
         self.conv3 = LRConvF2F3d(4, 8, 3, padding=1)
+        self.norm3 = Norm(8, affine=affine)
+        # self.norm3 = torch.nn.GroupNorm(8, 2 * 8, affine=True)
         self.conv4 = LRConvF2I3d(8, out_channels, 3, padding=1)
 
     def forward(self, input):
         output = self.conv1(input)
+        output = self.norm1(output)
         output = relu(output)
         output = self.conv2(output)
+        output = self.norm2(output)
         output = relu(output)
         output = self.conv3(output)
+        output = self.norm3(output)
         output = relu(output)
         output = self.conv4(output)
         return output
@@ -33,37 +46,49 @@ class Net2(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.conv1 = torch.nn.Conv3d(in_channels, 4, 3, padding=1)
+        self.norm1 = torch.nn.BatchNorm3d(4)
         self.conv2 = torch.nn.Conv3d(4, 8, 3, padding=1)
+        self.norm2 = torch.nn.BatchNorm3d(8)
         self.conv3 = torch.nn.Conv3d(8, 16, 3, padding=1)
+        self.norm3 = torch.nn.BatchNorm3d(16)
         self.conv4 = torch.nn.Conv3d(16, out_channels, 3, padding=1)
 
     def forward(self, input):
         output = self.conv1(input)
+        output = self.norm1(output)
         output = relu(output)
         output = self.conv2(output)
+        output = self.norm2(output)
         output = relu(output)
         output = self.conv3(output)
+        output = self.norm3(output)
         output = relu(output)
         output = self.conv4(output)
         return output
 
 
 filename = 'image.nii.gz'
-data = nib.load(filename).get_data()
+data = np.abs(nib.load(filename).get_data())
+data = (data / 1000).astype(int)
 data = torch.from_numpy(data).float()[None, None, ...].cuda()
+# data = torch.arange(27).float().view(1, 1, 3, 3, 3).cuda()
 flipped_data = data.flip(2)
 
 net1 = Net1(1, 1).cuda()
 output1 = net1(data).detach().cpu().numpy()
 output1_r = net1(flipped_data).detach().cpu().numpy()
 diff1 = np.abs(output1[:, :, ::-1, ...] - output1_r)
+diff1_tmp = diff1 / output1_r
+diff1_tmp = diff1_tmp[np.logical_not(np.isnan(diff1_tmp))]
 
 net2 = Net2(1, 1).cuda()
 output2 = net2(data).detach().cpu().numpy()
 output2_r = net2(flipped_data).cpu().detach().numpy()
 diff2 = np.abs(output2[:, :, ::-1, ...] - output2_r)
+diff2_tmp = diff2 / output2_r
+diff2_tmp = diff2_tmp[np.logical_not(np.isnan(diff2_tmp))]
 
-slice_ind = 100
+slice_ind = data.shape[-1] // 2
 data = data.cpu().numpy()
 flipped_data = flipped_data.cpu().numpy()
 
@@ -78,8 +103,8 @@ plt.subplot(3, 2, 4)
 plt.imshow(output1_r[0, 0, ...,  slice_ind], cmap='gray')
 plt.subplot(3, 2, 5)
 plt.imshow(diff1[0, 0, ..., slice_ind])
-text = 'max abs diff %.2e\noutput intensity [%.2e, %.2e]'
-text = text %  (np.max(diff1), np.min(output1), np.max(output1))
+text = 'max abs diff %.2e\nmax relative diff %.2e\noutput intensity [%.2e, %.2e]'
+text = text %  (np.max(diff1), np.max(diff1_tmp), np.min(output1), np.max(output1))
 plt.gcf().text(.5, .01, text, ha='center')
 
 plt.figure()
@@ -93,8 +118,8 @@ plt.subplot(3, 2, 4)
 plt.imshow(output2_r[0, 0, ...,  slice_ind], cmap='gray')
 plt.subplot(3, 2, 5)
 plt.imshow(diff2[0, 0, ..., slice_ind])
-text = 'max abs diff %.2e\noutput intensity [%.2e, %.2e]'
-text = text %  (np.max(diff2), np.min(output2), np.max(output2))
+text = 'max abs diff %.2e\nmax relative diff %.2e\noutput intensity [%.2e, %.2e]'
+text = text %  (np.max(diff2), np.max(diff2_tmp), np.min(output2), np.max(output2))
 plt.gcf().text(.5, .01, text, ha='center')
 
 plt.show()
